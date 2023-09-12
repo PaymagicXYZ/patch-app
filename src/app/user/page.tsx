0ef4369d5ca58aa97cb34903e93f67f269ae6b6f
Xinchen Liu <mike000708@me.com>
2023-09-12 10:03:51 -0400

    add get user

---
+++ src/app/user/page.tsx
@@ -0,0 +1,33 @@
+import { currentUser } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/api";
+
+export default async function Page() {
+  const user: User | null = await currentUser();
+  if (user) {
+    return (
+      <main className="m-4">
+        <h1 className="text-xl">Patch wallets associated with this account:</h1>
+        <div>
+          <ul>
+            <li>patch-test:{user.username}</li>
+            {user.emailAddresses.map((email, i) => (
+              <li key={i}>email:{email.emailAddress}</li>
+            ))}
+            {user.phoneNumbers.map((tel, i) => (
+              <li key={i}>tel:{tel.phoneNumber}</li>
+            ))}
+            {user.externalAccounts.map((account, i) => (
+              <li key={i}>
+                {account.provider.split("_")[1]}:{account.username}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <h1 className="text-xl">User Object:</h1>
+        <pre className="w-full bg-gray-100 p-4 rounded-md">
+          {JSON.stringify(user, null, 2)}
+        </pre>
+      </main>
+    );
+  }
+}
