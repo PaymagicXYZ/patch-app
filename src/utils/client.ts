a63e921031e9be82b1856cdeee4d13a6cef83996
Xinchen Liu <mike000708@me.com>
2024-01-05 12:21:12 -0500

    replace api calls with SDK

---
+++ src/utils/client.ts
@@ -0,0 +1,7 @@
+import Client from "@patchwallet/patch-sdk";
+
+const credentials = {
+  clientId: process.env.CLIENT_ID!,
+  clientSecret: process.env.CLIENT_SECRET!,
+};
+export const client = new Client(credentials);
--- src/utils/getUser.ts
+++
@@ -1,9 +0,0 @@
-export const getUser = async (user: string) => {
-  return await fetch(`https://api.clerk.com/v1/users/${user}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.CLERK_SECRET_KEY}`,
-    },
-  }).then((res) => res.json());
-};
