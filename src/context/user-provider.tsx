b08697c48a25f04324f1635a12cb217c447249e7
Xinchen Liu <mike000708@me.com>
2024-01-16 10:11:51 -0500

    Add UserProvider component to handle user context

---
+++ src/context/user-provider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Chain } from "@patchwallet/patch-sdk";
+import { SetStateAction, createContext, useState } from "react";
+
+interface UserContextType {
+  user: string;
+  setUser: React.Dispatch<React.SetStateAction<string>>;
+  chain: Chain;
+  setChain: React.Dispatch<React.SetStateAction<Chain>>;
+}
+
+export const UserContext = createContext<UserContextType>({
+  user: "",
+  setUser: function (value: SetStateAction<string>): void {
+    throw new Error("Function not implemented.");
+  },
+  chain: "matic",
+  setChain: function (value: SetStateAction<Chain>): void {
+    throw new Error("Function not implemented.");
+  },
+});
+
+export default function UserProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [user, setUser] = useState("");
+  const [chain, setChain] = useState<Chain>("matic");
+  return (
+    <UserContext.Provider value={{ user, setUser, chain, setChain }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
