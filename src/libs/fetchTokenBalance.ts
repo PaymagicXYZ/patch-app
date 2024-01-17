62f1cc26d94713c410f412bb3f0a3c841fc3641c
Xinchen Liu <mike000708@me.com>
2024-01-17 12:05:28 -0500

    Add fetchTokenBalance function to fetch token balance from Covalent API

---
+++ src/libs/fetchTokenBalance.ts
@@ -0,0 +1,22 @@
+import "server-only";
+import { getChainNameFromChainTag } from "@/utils/chain";
+import { client } from "./client";
+import type { UserId, Chain } from "@patchwallet/patch-sdk";
+
+export async function fetchTokenBalance(userId: UserId, chain: Chain) {
+  const address = await client.resolve(userId);
+  const chainName = getChainNameFromChainTag(chain);
+  if (chainName) {
+    const url = `https://api.covalenthq.com/v1/${chainName}/address/${address}/balances_v2/`;
+    const res = await fetch(url, {
+      headers: {
+        "Accept-Encoding": "*",
+        "Content-Type": "application/json",
+        Authorization: "Basic " + process.env.COVALENT_API_KEY,
+      },
+    }).then((res) => res.json());
+    return res.data;
+  } else {
+    return null;
+  }
+}
