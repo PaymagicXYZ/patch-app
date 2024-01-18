98acb20a99c917a818585b64cffc742edceec334
Xinchen Liu <mike000708@me.com>
2024-01-18 10:48:42 -0500

    Add fetchNFTAssets function to fetch NFT assets from different networks

---
+++ src/libs/fetchNFTAssets.ts
@@ -0,0 +1,30 @@
+import {
+  getChainNameFromChainTag,
+  getNetworkfromChainTag,
+} from "@/utils/chain";
+import { UserId, Chain } from "@patchwallet/patch-sdk";
+import { client } from "./client";
+
+export const fetchNFTAssets = async (userId: UserId, chain: Chain) => {
+  const address = await client.resolve(userId);
+  const network = getNetworkfromChainTag(chain);
+  if (network) {
+    const ownedAssetsURL = `https://${network}.g.alchemy.com/nft/v3/${process.env.ALCHEMY_API_KEY}/getNFTsForOwner?owner=${address}&withMetadata=true&excludeFilters\[\]=SPAM`;
+    return fetch(ownedAssetsURL)
+      .then((res) => res.json())
+      .then(async (data) => {
+        return data.ownedNfts;
+      });
+  } else {
+    const chainName = getChainNameFromChainTag(chain);
+    const nftURL = `https://api.covalenthq.com/v1/${chainName}/address/${address}/balances_nft/`;
+    const data = await fetch(nftURL, {
+      headers: {
+        "Accept-Encoding": "*",
+        "Content-Type": "application/json",
+        Authorization: "Basic " + process.env.COVALENT_API_KEY,
+      },
+    }).then((res) => res.json());
+    return data;
+  }
+};
