706705ab2e490098113591a3cce7ed2669a76331
Xinchen Liu <mike000708@me.com>
2024-01-22 10:23:50 -0500

    Add useEffect and useRouter to AccountSelector component

--- src/components/AccountSelector.tsx
+++ src/components/AccountSelector.tsx
@@ -1,6 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import type { UserId } from "@patchwallet/patch-sdk";
+import { useRouter, usePathname } from "next/navigation";
+import isUserId from "@/utils/checkUserId";
 
 import { SignBtn } from "./SignBtn";
 
@@ -13,22 +15,38 @@ const AccountSelector = ({
   wallets: string[];
   token: string;
 }) => {
-  const [selectedAccount, setSelectedAccount] = useState("");
+  const router = useRouter();
+  const pathname = usePathname();
+  const [selectedAccount, setSelectedAccount] = useState<UserId | "">("");
+  const user = pathname.split("/")[2];
+  const chain = pathname.split("/")[3] || "";
+  useEffect(() => {
+    if (isUserId(user) && availableWallets.lastIndexOf(user) > -1) {
+      setSelectedAccount(user);
+    }
+  }, [availableWallets, user]);
 
   return (
     <div>
       <select
         value={selectedAccount}
-        onChange={(e) => setSelectedAccount(e.target.value)}
+        onChange={(e) => {
+          setSelectedAccount((e.target.value as UserId) || "");
+          router.push(`/user/${e.target.value}/${chain}`);
+        }}
       >
+        <option value="" disabled>
+          Select a wallet
+        </option>
         {availableWallets.map((account, i) => (
           <option key={i} value={account}>
             {account}
           </option>
         ))}
       </select>
-      <pre>{wallets[availableWallets.indexOf(selectedAccount as UserId)]}</pre>
-      <SignBtn userId={selectedAccount as UserId} token={token} />
+      {selectedAccount && (
+        <pre>{wallets[availableWallets.indexOf(selectedAccount)]}</pre>
+      )}
     </div>
   );
 };
