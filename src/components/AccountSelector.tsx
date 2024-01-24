75a8bc9940d58e8eb4577b4453dbe8eb54c458df
Xinchen Liu <mike000708@me.com>
2024-01-24 12:38:13 -0500

    Refactor AccountSelector component for new routes

--- src/components/AccountSelector.tsx
+++ src/components/AccountSelector.tsx
@@ -4,22 +4,18 @@ import type { UserId } from "@patchwallet/patch-sdk";
 import { useRouter, usePathname } from "next/navigation";
 import isUserId from "@/utils/checkUserId";
 
-import { SignBtn } from "./SignBtn";
-
 const AccountSelector = ({
   availableWallets,
   wallets,
-  token,
 }: {
   availableWallets: UserId[];
   wallets: string[];
-  token: string;
 }) => {
   const router = useRouter();
   const pathname = usePathname();
   const [selectedAccount, setSelectedAccount] = useState<UserId | "">("");
-  const user = pathname.split("/")[2];
-  const chain = pathname.split("/")[3] || "";
+  const user = pathname.split("/")[1];
+  const chain = pathname.split("/")[2] || "";
   useEffect(() => {
     if (isUserId(user) && availableWallets.lastIndexOf(user) > -1) {
       setSelectedAccount(user);
@@ -32,7 +28,7 @@ const AccountSelector = ({
         value={selectedAccount}
         onChange={(e) => {
           setSelectedAccount((e.target.value as UserId) || "");
-          router.push(`/user/${e.target.value}/${chain}`);
+          router.push(`/${e.target.value}/${chain}`);
         }}
       >
         <option value="" disabled>
