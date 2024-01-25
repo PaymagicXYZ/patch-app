3bbe3f09d7dccf1f2f8b4629d432a5e223c760dd
Xinchen Liu <mike000708@me.com>
2024-01-25 10:33:22 -0500

    Refactor Nav component to conditionally render UserButton or SignInButton based on user authentication status

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,15 @@
 import UserDetail from "@/components/User";
-import { UserButton } from "@clerk/nextjs";
+import { auth, UserButton, SignInButton } from "@clerk/nextjs";
 
 export default function Nav() {
+  const { userId } = auth();
   return (
     <nav className="flex flex-row items-right p-2">
-      <UserButton afterSignOutUrl="/" />
+      {userId ? (
+        <UserButton afterSignOutUrl="/" />
+      ) : (
+        <SignInButton afterSignInUrl={`/user`} />
+      )}
       <UserDetail />
     </nav>
   );
