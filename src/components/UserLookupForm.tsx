f0335ba3eeef35930f13559c766cac1d8f47ce25
ealeksandrov7 <evlogi@weichain.tech>
2024-01-31 14:34:27 +0200

    refactor: user lookup component structure

--- src/components/UserLookupForm.tsx
+++ src/components/UserLookupForm.tsx
@@ -9,6 +9,7 @@ import { useDebouncedCallback } from 'use-debounce';
 import { useContext } from "react";
 import { UserContext } from "@/context/user-provider";
 import { SupportedSocialNetworkIds } from "@/types";
+import { useAppendQueryParams } from "@/hooks/useAppendQueryParams";
 
 
 export const SearchUser = ({className, ...props}: InputProps) => {
@@ -19,29 +20,13 @@ export const SearchUser = ({className, ...props}: InputProps) => {
   // Note: If the user hasn't selected a provider, it should default to 'twitter'
   const lookupProviderId = (searchParams.get("provider")?.toString() as SupportedSocialNetworkIds) ?? "twitter"
   const lookupProviderDetails = getSupportedLookupNetworks()[lookupProviderId]
+  const {appendQueryParams} = useAppendQueryParams()
 
-  
-  const handleSelect = (term: string) => {
-    const params = new URLSearchParams(searchParams);
-
-    if (term) {
-      params.set('provider', term);
-    } else {
-      params.delete('provider');
-    }
+  const handleOnQueryUpdate = (key: string, value: string) => appendQueryParams(key, value, (params) => {
     replace(`${pathname}?${params.toString()}`);
-  }
+  })
 
-  const handleSearch = useDebouncedCallback((term: string) => {
-    const params = new URLSearchParams(searchParams);
-
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  }, 300)
+  const withDebounce = useDebouncedCallback(handleOnQueryUpdate, 300)
 
   const handleOnSubmit = () => {
     const params = new URLSearchParams(searchParams);
@@ -60,8 +45,8 @@ export const SearchUser = ({className, ...props}: InputProps) => {
 
     return (
       <div className="flex w-full gap-2 sm:w-4/6 sm:max-w-[520px]">
-          <Input defaultValue={queryString} onChange={(e) => handleSearch(e.target.value)} leftButton={<SelectSocialProvider onChange={handleSelect} />} type="text" placeholder={lookupProviderDetails.placeholder} className={cn("mt-4 w-[360px] border-gray-800 bg-gray-950 focus:border-[0.5px] focus:bg-gray-1000", className)} {...props} />
-          <Button onClick={handleOnSubmit} disabled={!queryString} className="rounded-lg bg-orange-100 text-gray-1000">Look up wallet</Button>
+        <Input defaultValue={queryString} onChange={(e) => withDebounce("query", e.target.value)} leftButton={<SelectSocialProvider onChange={(val) => handleOnQueryUpdate("provider", val)} />} type="text" placeholder={lookupProviderDetails.placeholder} className={cn("mt-4 w-[360px] border-gray-800 bg-gray-950 focus:border-[0.5px] focus:bg-gray-1000", className)} {...props} />
+        <Button onClick={handleOnSubmit} disabled={!queryString} className="rounded-lg bg-orange-100 text-gray-1000">Look up wallet</Button>
       </div>
     )
 }
