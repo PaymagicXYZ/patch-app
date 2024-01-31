5950982a36232dee11566cae0609f63e128bfdf4
ealeksandrov7 <evlogi@weichain.tech>
2024-01-31 16:08:15 +0200

    misc: revert middleware

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,7 +2,7 @@ import WalletDetail from '@/components/WalletDetail';
 import { Suspense } from 'react';
 export default async function Page() {
   return (
-    <main className="m-4">
+    <main className="flex-1">
       <h1 className="text-xl">Patch wallets associated with this account:</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <WalletDetail />
