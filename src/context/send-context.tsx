b2c705affe1533b76a97cc25aef9aab754bf58d9
ealeksandrov7 <evlogi@weichain.tech>
2024-02-06 16:34:20 +0200

    fix: deploy issue

--- src/context/send-context.tsx
+++
@@ -1,9 +0,0 @@
-import { Address } from "@patchwallet/patch-sdk";
-import { create } from "zustand";
-
-const useBearStore = create<{ to: Address }>((set) => ({
-  to: "0",
-  setTo: (to: Address) => set({ to }),
-  //   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  //   removeAllBears: () => set({ bears: 0 }),
-}));
