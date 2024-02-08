5bf3ae88c5de3527e51c98f06b7eddf31a743304
ealeksandrov7 <evlogi@weichain.tech>
2024-02-08 17:03:10 +0200

    feat: add Alpha alert banner

--- src/app/[userId]/[chain]/page.tsx
+++ src/app/[userId]/[chain]/page.tsx
@@ -13,17 +13,18 @@ export default async function Wallet({
 
   return (
     <main className="flex-1">
-      {/* <AlphaBanner /> */}
       <div>
         <div className="relative mx-5 flex h-full flex-col md:mx-10">
           <div className="mb-auto mt-6 flex justify-center md:mt-[108px] ">
             <div className="flex w-full flex-col gap-4 md:w-[600px]">
               <div className="flex basis-0 flex-col flex-wrap md:flex-nowrap">
-                <ProfileWidget
-                  userId={_userId as UserId}
-                  chain={params.chain}
-                  className="w-full"
-                />
+                <Suspense>
+                  <ProfileWidget
+                    userId={_userId as UserId}
+                    chain={params.chain}
+                    className="w-full"
+                  />
+                </Suspense>
               </div>
               <div>
                 <Suspense fallback={<InventoryTabsSkeleton />}>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,27 +6,31 @@ import walletLogo from "../../public/wallet_logo.svg";
 import { HeaderLookupForm } from "./UserLookupForm";
 import Link from "next/link";
 import { HeaderSkeleton } from "./Skeleton";
+import AlphaBanner from "./AlphaBanner";
 
 export default function Nav() {
   return (
-    <nav className="flex w-full flex-row-reverse flex-wrap-reverse items-center gap-4 p-4 md:flex-wrap">
-      <div className="flex flex-1 flex-row-reverse items-center gap-4 text-right">
-        <Suspense fallback={<HeaderSkeleton />}>
-          <NavSignIn />
-          <UserDetail />
-        </Suspense>
-      </div>
-      <div className="flex items-center gap-4">
-        <Link href="/">
-          <Image
-            src={walletLogo}
-            alt="PatchWallet"
-            className="cursor-pointer md:block"
-            width={168}
-          />
-        </Link>
-        <HeaderLookupForm />
-      </div>
-    </nav>
+    <>
+      <AlphaBanner />
+      <nav className="flex w-full flex-row-reverse flex-wrap-reverse items-center gap-4 p-4 pt-16 md:flex-wrap md:pt-12">
+        <div className="flex flex-1 flex-row-reverse items-center gap-4 text-right">
+          <Suspense fallback={<HeaderSkeleton />}>
+            <NavSignIn />
+            <UserDetail />
+          </Suspense>
+        </div>
+        <div className="flex items-center gap-4">
+          <Link href="/">
+            <Image
+              src={walletLogo}
+              alt="PatchWallet"
+              className="cursor-pointer md:block"
+              width={168}
+            />
+          </Link>
+          <HeaderLookupForm />
+        </div>
+      </nav>
+    </>
   );
 }
