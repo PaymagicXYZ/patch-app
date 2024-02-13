c2adc964ae7fe286bf5f6edff8ae9fb1611aee66
Xinchen Liu <mike000708@me.com>
2024-02-13 09:13:47 -0500

    Add signMoonPayURL functions for signing MoonPay URLs

---
+++ src/libs/actions/signMoonPayURL.ts
@@ -0,0 +1,31 @@
+"use server";
+import crypto from "crypto";
+
+export async function signedMoonPayURL(
+  currency: string,
+  walletAddress: string
+) {
+  const originalUrl = `https://buy${
+    process.env.NODE_ENV !== "production" && "-sandbox"
+  }.moonpay.com?apiKey=${
+    process.env.NEXT_PUBLIC_MOONPAY_PUBLIC_KEY
+  }&currencyCode=${currency}&walletAddress=${walletAddress}`;
+  const signature = crypto
+    .createHmac("sha256", process.env.MOONPAY_SECRET!)
+    .update(new URL(originalUrl).search)
+    .digest("base64");
+  const urlWithSignature = `${originalUrl}&signature=${encodeURIComponent(
+    signature
+  )}`;
+  console.log(urlWithSignature);
+  return urlWithSignature;
+}
+
+export async function signMoonPayURL(url: string) {
+  const originalUrl = url;
+  const signature = crypto
+    .createHmac("sha256", process.env.MOONPAY_SECRET!)
+    .update(new URL(originalUrl).search)
+    .digest("base64");
+  return signature;
+}
