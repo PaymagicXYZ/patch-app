5ad58eebeea6325f4ed364c38d33d5520451fd37
Xinchen Liu <mike000708@me.com>
2024-02-20 12:50:01 -0500

    Add revalidate and dynamic variables to Wallet page

--- src/app/[userId]/[chain]/page.tsx
+++ src/app/[userId]/[chain]/page.tsx
@@ -4,6 +4,9 @@ import { AssetsTab } from "@/app/[userId]/[chain]/components/AssetsTabs";
 import { Suspense } from "react";
 import { InventoryTabsSkeleton } from "@/components/Skeleton";
 
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
+
 export default async function Wallet({
   params,
 }: {
@@ -18,7 +21,11 @@ export default async function Wallet({
           <div className="mb-auto mt-6 flex justify-center md:mt-[108px] ">
             <div className="flex w-full flex-col gap-4 md:w-[600px]">
               <div className="flex basis-0 flex-col flex-wrap md:flex-nowrap">
-                <Suspense fallback={<div className="w-full md:w-[600px] h-[328px] rounded-2xl bg-gray-900"></div>}>
+                <Suspense
+                  fallback={
+                    <div className="h-[328px] w-full rounded-2xl bg-gray-900 md:w-[600px]"></div>
+                  }
+                >
                   <ProfileWidget
                     userId={_userId as UserId}
                     chain={params.chain}
