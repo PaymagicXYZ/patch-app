9a1d35e85ce0797feab90e7b69308590a6b72947
Xinchen Liu <mike000708@me.com>
2024-02-26 13:46:50 -0700

    Fix formatting issues and enforce prettier

--- src/libs/actions/tokens.ts
+++ src/libs/actions/tokens.ts
@@ -7,52 +7,52 @@ import { sortCovalentAssetsByType } from "../utils";
 import { unstable_cache } from "next/cache";
 
 export const fetchTokenBalance = unstable_cache(
-	async (
-		address: Address,
-		chain: Chain,
-		withNFTs = false,
-		includeFiatValue = false,
-	) => {
-		const chainName = getChainNameFromShortName(chain);
+  async (
+    address: Address,
+    chain: Chain,
+    withNFTs = false,
+    includeFiatValue = false,
+  ) => {
+    const chainName = getChainNameFromShortName(chain);
 
-		if (chainName) {
-			const response =
-				await covalentInstance.BalanceService.getTokenBalancesForWalletAddress(
-					chainName,
-					address as Address,
-					{
-						nft: withNFTs,
-					},
-				);
+    if (chainName) {
+      const response =
+        await covalentInstance.BalanceService.getTokenBalancesForWalletAddress(
+          chainName,
+          address as Address,
+          {
+            nft: withNFTs,
+          },
+        );
 
-			const sortedAssets = sortCovalentAssetsByType(
-				response?.data?.items || [],
-				includeFiatValue,
-			);
+      const sortedAssets = sortCovalentAssetsByType(
+        response?.data?.items || [],
+        includeFiatValue,
+      );
 
-			return {
-				data: sortedAssets,
-				error: response.error_message,
-				fiatBalance: sortedAssets.fiatValue,
-			};
-		}
+      return {
+        data: sortedAssets,
+        error: response.error_message,
+        fiatBalance: sortedAssets.fiatValue,
+      };
+    }
 
-		return { data: { nfts: [], tokens: [] }, error: "Invalid Chain" };
-	},
-	["fetch_tokens"],
-	{
-		tags: ["token_balance"],
-		revalidate: 300,
-	},
+    return { data: { nfts: [], tokens: [] }, error: "Invalid Chain" };
+  },
+  ["fetch_tokens"],
+  {
+    tags: ["token_balance"],
+    revalidate: 300,
+  },
 );
 
 export const fetchFiatBalance = unstable_cache(
-	async (address: Address, chain: Chain) => {
-		return (await fetchTokenBalance(address, chain, false, true)).fiatBalance;
-	},
-	["fetch_fiat"],
-	{
-		tags: ["fiat_balance"],
-		revalidate: 300,
-	},
+  async (address: Address, chain: Chain) => {
+    return (await fetchTokenBalance(address, chain, false, true)).fiatBalance;
+  },
+  ["fetch_fiat"],
+  {
+    tags: ["fiat_balance"],
+    revalidate: 300,
+  },
 );
