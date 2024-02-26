b07cd9bd4a508f8bf0133ba36d36ebf48df23b36
Xinchen Liu <mike000708@me.com>
2024-02-26 13:46:06 -0700

    add max duration for server actions

--- src/libs/actions/sign.ts
+++ src/libs/actions/sign.ts
@@ -3,6 +3,7 @@
 import { client } from "../client";
 import type { UserId } from "@patchwallet/patch-sdk";
 
+export const maxDuration = 300;
 export async function signMsg(userId: UserId, message: string, auth: string) {
   const sig = await client.sign({
     userId,
--- src/libs/actions/tx.ts
+++ src/libs/actions/tx.ts
@@ -3,41 +3,42 @@ import type { UserId, Chain, Address, HexString } from "@patchwallet/patch-sdk";
 import { auth } from "@clerk/nextjs";
 import { client } from "../client";
 
+export const maxDuration = 300;
 export async function sendTx(
-	chain: Chain,
-	userId: UserId,
-	to: Address[],
-	value: string[],
-	data: HexString[],
+  chain: Chain,
+  userId: UserId,
+  to: Address[],
+  value: string[],
+  data: HexString[],
 ) {
-	const { getToken } = auth();
-	const _token = await getToken({
-		template: "patchwallet",
-	});
+  const { getToken } = auth();
+  const _token = await getToken({
+    template: "patchwallet",
+  });
 
-	if (!_token) {
-		throw new Error("Not authenticated");
-	}
+  if (!_token) {
+    throw new Error("Not authenticated");
+  }
 
-	const tx = await client.tx({
-		userId,
-		chain,
-		to,
-		value,
-		data,
-		delegatecall: 0,
-		auth: _token,
-	});
+  const tx = await client.tx({
+    userId,
+    chain,
+    to,
+    value,
+    data,
+    delegatecall: 0,
+    auth: _token,
+  });
 
-	if ("txHash" in tx) {
-		return {
-			txHash: tx.txHash,
-		};
-	}
+  if ("txHash" in tx) {
+    return {
+      txHash: tx.txHash,
+    };
+  }
 
-	if ("error" in tx) {
-		return {
-			error: tx.error,
-		};
-	}
+  if ("error" in tx) {
+    return {
+      error: tx.error,
+    };
+  }
 }
